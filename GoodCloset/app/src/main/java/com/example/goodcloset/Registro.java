6c8dac5969e37ce7ccfb542ac6fb395183a054dc
MarioCastilloArenas <127202707+MarioCastilloArenas@users.noreply.github.com>
2024-02-05 20:01:56 +0100

    ANDROID STUDIO BIEN
    GITIGNORE MODIFICADO

---
+++ GoodCloset/app/src/main/java/com/example/goodcloset/Registro.java
@@ -0,0 +1,67 @@
+package com.example.goodcloset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class Registro extends AppCompatActivity {
+
+    ImageView imgbg;
+    TextView txtView,name,surname,email,username,password, button;
+    LinearLayout ly;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        imgbg = findViewById(R.id.registrobg);
+        txtView = findViewById(R.id.title);
+        ly = findViewById(R.id.linearLayout);
+        name = findViewById(R.id.name);
+        surname = findViewById(R.id.apellido);
+        email = findViewById(R.id.email);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        button = findViewById(R.id.enviar);
+
+        imgbg.animate().translationY(-2100).setDuration(1000).setStartDelay(0);
+
+        Animation aniFade = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slideleft);
+        txtView.startAnimation(aniFade);
+
+        Animation aniFade2 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slideright);
+        name.startAnimation(aniFade2);
+
+        Animation aniFade3 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slideleft);
+        surname.startAnimation(aniFade3);
+
+        Animation aniFade4 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slideright);
+        email.startAnimation(aniFade4);
+
+        Animation aniFade5 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slideleft);
+        username.startAnimation(aniFade5);
+
+        Animation aniFade6 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slideright);
+        password.startAnimation(aniFade6);
+
+        Animation aniFade7 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fadein);
+        button.startAnimation(aniFade7);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Registro.this, LogIn.class));
+                finish();
+            }
+        }, 4500);
+
+    }
+}
---
+++ GoodCloset/app/src/main/java/com/example/goodcloset/SplashScreen.java
@@ -0,0 +1,55 @@
+package com.example.goodcloset;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView logo, imgbg;
+    TextView appname,powered;
+    LottieAnimationView lottieAnimationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //logo = findViewById(R.id.logo);
+        imgbg = findViewById(R.id.img);
+        appname = findViewById(R.id.appName);
+        lottieAnimationView = findViewById(R.id.lottie);
+        powered = findViewById(R.id.powered);
+
+        imgbg.animate().translationY(-2500).setDuration(800).setStartDelay(3000);
+        //logo.animate().translationY(-1400).setDuration(1000).setStartDelay(4000);
+        appname.animate().translationY(-1400).setDuration(800).setStartDelay(3000);
+        lottieAnimationView.animate().translationY(-1700).setDuration(800).setStartDelay(3000);
+        powered.animate().translationY(-1900).setDuration(800).setStartDelay(3000);
+
+        final Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.slideleft);
+        final Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.slideright);
+        final Animation animacion3 = AnimationUtils.loadAnimation(this, R.anim.slideup);
+
+        appname.startAnimation(animacion2);
+        lottieAnimationView.startAnimation(animacion1);
+        powered.startAnimation(animacion3);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, Registro.class));
+                finish();
+            }
+        }, 4100);
+
+    }
+}
