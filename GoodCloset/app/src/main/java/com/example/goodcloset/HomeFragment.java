151875787f76bce151a8f8c702601cb153445826
MarioCastilloArenas <127202707+MarioCastilloArenas@users.noreply.github.com>
2024-02-07 17:22:49 +0100

    PUSH CAMARA
    CHICOS AQUI VA TODA LA FUINCIONALIDAD DE LA CAMARA, SIN RETROFIT, Y ADEMAS NO VA NADA DE LA API DE MIGUEL, EN EL PULL NO ME LO HA TRAIDO

--- GoodCloset/app/src/main/java/com/example/goodcloset/HomeFragment.java
+++ GoodCloset/app/src/main/java/com/example/goodcloset/HomeFragment.java
@@ -17,30 +17,48 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
-import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     Button btnCamara;
-    ImageView imgView;
+    ImageView partearriba, partemedia, parteabajo;
+
+    private ArrayList<Bitmap> imagenesCapturadas;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        btnCamara = rootView.findViewById(R.id.btnCamara);
-        imgView = rootView.findViewById(R.id.imageView);
+        imagenesCapturadas = new ArrayList<>();
 
-        btnCamara.setOnClickListener(new View.OnClickListener() {
+        partearriba = rootView.findViewById(R.id.parteAriba);
+        partemedia = rootView.findViewById(R.id.parteMedia);
+        parteabajo = rootView.findViewById(R.id.parteAbajo);
+
+        partearriba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                camaraLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+                camaraLauncher1.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+            }
+        });
+        partemedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                camaraLauncher2.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+            }
+        });
+        parteabajo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                camaraLauncher3.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
             }
         });
         return rootView;
     }
 
-    ActivityResultLauncher<Intent> camaraLauncher = registerForActivityResult(new
+    ActivityResultLauncher<Intent> camaraLauncher1 = registerForActivityResult(new
             ActivityResultContracts.StartActivityForResult(), new
             ActivityResultCallback<ActivityResult>() {
                 @Override
@@ -48,14 +66,36 @@ public class HomeFragment extends Fragment {
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         Bundle extras = result.getData().getExtras();
                         Bitmap imgBitmap = (Bitmap) extras.get("data");
+                        partearriba.setImageBitmap(imgBitmap);
+                        imagenesCapturadas.add(imgBitmap);
+                    }
+                }
+            });
 
-                        // Convertir la imagen capturada en un array de bytes
-                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                        imgBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
-                        byte[] byteArray = stream.toByteArray();
+    ActivityResultLauncher<Intent> camaraLauncher2 = registerForActivityResult(new
+            ActivityResultContracts.StartActivityForResult(), new
+            ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Bundle extras = result.getData().getExtras();
+                        Bitmap imgBitmap = (Bitmap) extras.get("data");
+                        partemedia.setImageBitmap(imgBitmap);
+                        imagenesCapturadas.add(imgBitmap);
+                    }
+                }
+            });
 
-
-
+    ActivityResultLauncher<Intent> camaraLauncher3 = registerForActivityResult(new
+            ActivityResultContracts.StartActivityForResult(), new
+            ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Bundle extras = result.getData().getExtras();
+                        Bitmap imgBitmap = (Bitmap) extras.get("data");
+                        parteabajo.setImageBitmap(imgBitmap);
+                        imagenesCapturadas.add(imgBitmap);
                     }
                 }
             });
