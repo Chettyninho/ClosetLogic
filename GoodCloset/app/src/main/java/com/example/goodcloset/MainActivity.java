f22ae61e0289ea1b4fb55ff4b0492c232af480b0
MarioCastilloArenas <127202707+MarioCastilloArenas@users.noreply.github.com>
2024-02-08 12:22:14 +0100

    main activity manager

--- GoodCloset/app/src/main/java/com/example/goodcloset/MainActivity.java
+++ GoodCloset/app/src/main/java/com/example/goodcloset/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.goodcloset;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment(), true);
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navHome) {
+
+                    loadFragment(new HomeFragment(), true);
+
+                } else if (itemId == R.id.navProfile) {
+
+                    loadFragment(new ProfileFragment(), false);
+
+                } else if (itemId == R.id.navAdd){ // nav Add
+
+                    loadFragment(new AddFragment(), false);
+
+                }
+                else {
+
+                    loadFragment(new SearchFragment(), false);
+
+                }
+
+                return true;
+            }
+        });
     }
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if(isAppInitialized){
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
