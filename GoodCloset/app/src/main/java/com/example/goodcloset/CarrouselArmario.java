5244c64b138e84561bd2af2c8b7483b771e0cb4d
Chettyninho <acedomarfil1a@gmail.com>
2024-02-18 17:24:05 +0100

    llegan las fotos a donde ruben

--- GoodCloset/app/src/main/java/com/example/goodcloset/CarrouselArmario.java
+++ GoodCloset/app/src/main/java/com/example/goodcloset/CarrouselArmario.java
@@ -64,8 +64,9 @@ public class CarrouselArmario extends AppCompatActivity  {
             public void onResponse(Call<List<OutfitModelo>> call, Response<List<OutfitModelo>> response) {
                 if (response.isSuccessful()){
                     List<OutfitModelo> listaOutfits =response.body();
-                    Log.d("responseBody Miguel Test", listaOutfits.toString());
-
+                    for (OutfitModelo o : listaOutfits){
+                        Log.d("responseBody Miguel Test", o.getPrendasDelOutfit().toString());
+                    }
                 }
             }
 
@@ -77,52 +78,4 @@ public class CarrouselArmario extends AppCompatActivity  {
         }
     }
 }
-
-/*
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import com.example.goodcloset.Adapter.CustomPagerAdapter;
-import com.example.goodcloset.Retrofit.ApiClient;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class CarrouselArmario extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.ver_outfits_armarios);
-
-        ViewPager viewPager = findViewById(R.id.viewPager);
-        Button button = findViewById(R.id.button3);
-
-        // Suponiendo que tienes un método en tu API para obtener la lista de imágenes
-        List<YourImageModel> imageListFromAPI = ApiClient.fetchImageListFromAPI("URL_DE_TU_API");
-
-        CustomPagerAdapter adapter = new CustomPagerAdapter(this, imageListFromAPI);
-        viewPager.setAdapter(adapter);
-    }
-
-    private List<YourImageModel> tuMetodoParaObtenerListaDeImagenesDesdeAPI() {
-        // Aquí deberías hacer la llamada a tu API para obtener la lista de imágenes
-        // Por ahora, simplemente creamos una lista de imágenes de ejemplo
-        List<YourImageModel> imageList = new ArrayList<>();
-
-        // Añade aquí tus imágenes obtenidas desde la API
-
-        return imageList;
-    }
-}
-*/
 
